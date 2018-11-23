a4ce145815bdcffde6acd27e990129375636deb3
SeenJOB <43837594+SeenJOB@users.noreply.github.com>
2018-11-22 19:31:53 -0500

    Validate lenght method

--- SQ_A2_TDD/Triangle.cs
+++ SQ_A2_TDD/Triangle.cs
@@ -21,6 +21,15 @@ namespace SQ_A2_TDD
             return valid;
         }
 
-
+        public bool validateAngles(int angleA, int angleB, int angleC)
+        {
+            bool valid = false;
+            int sum = angleA + angleB + angleC;
+            if (sum == 180)
+            {
+                valid = true;
+            }
+            return valid;
+        }
     }
 }
