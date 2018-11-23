79500d7a3a13d8647929c6a3b6ee76be6f16a8a4
SeenJOB <43837594+SeenJOB@users.noreply.github.com>
2018-11-22 19:11:27 -0500

    Validate side lenght fail test

--- SQ_A2_TDD.Test/TriangleTest.cs
+++ SQ_A2_TDD.Test/TriangleTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SQ_A2_TDD;
+using System.Text.RegularExpressions;
 
 namespace SQ_A2_TDD.Test
 {
@@ -11,6 +12,14 @@ namespace SQ_A2_TDD.Test
         public void TestClassExist()
         {
             var Tri = new Triangle();
+        }
+
+        [TestMethod]
+        public void TestSideValid()
+        {
+            Triangle Tri = new Triangle();
+            bool test = Tri.validateLenth(-5);      
+            Assert.IsTrue(test);
         }
     }
 }
